3eefc4f15f29494c8cef10b735e313c3e5c9b549
KemboiK <kipropkelvin374@gmail.com>
2024-08-29 23:29:23 +0300

    Added project-list functionality and updated templates

--- Yearbook/Projects/urls.py
+++ Yearbook/Projects/urls.py
@@ -1,7 +1,11 @@
 from . import views
 from django.urls import path 
+from .views import Project_list
+from .views import home
+from Projects.views import root_view
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    
+    path('Projects/', home, name='home'),  # Home page URL
+    path("Projects/", Project_list, name="Project-list"),
+    path('', root_view, name='root'), #Root URL pattern
 ]
--- Yearbook/Projects/views.py
+++ Yearbook/Projects/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import Project
 
-# Create your views here.
+
+
+# HTML rendering view
 def home(request):
-    context={}
-    return render(request, "Projects/home.html", context)
+    return render(request, 'Projects/home.html')
+
+def root_view(request):
+    return render(request, 'home.html')
+
+def Project_list(request):
+    Projects = Project.objects.all()
+    context = {'Projects': Projects}
+    return render(request, "Projects/Project_list.html", context)
